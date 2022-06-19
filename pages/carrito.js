927814d42d6d8f06e41a272c11b3da45ff3a8e4b
AlexDays14 <al02954463@tecmilenio.mx>
2022-06-19 02:22:32 -0600

    10

--- pages/carrito.js
+++ pages/carrito.js
@@ -44,7 +44,7 @@ const Carrito = ({carrito, actualizarCantidad, eliminarProducto}) => {
                                             <option value="7">7</option>
                                             <option value="8">8</option>
                                             <option value="9">9</option>
-                                            <option value="10">7</option>
+                                            <option value="10">10</option>
                                         </select>
                                     </div>
 
